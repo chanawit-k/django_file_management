ea329319961e11cb08b576194efe546fd9ad656b
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2024-05-01 16:29:26 +0700

    - add model

--- django_file_management/uploadfile/models.py
+++ django_file_management/uploadfile/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    cv_file = models.FileField(upload_to='employee_files/cv/', null=True, blank=True)
+    photo_file = models.FileField(upload_to='employee_files/photo/', null=True, blank=True)
+
+def certificate_upload_path(instance,filename):
+    return f'employee_files/certificates/{instance.employee.id}/{filename}'
+
+class EmployeeCertificate(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    certificate_file = models.FileField(upload_to=certificate_upload_path, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.employee}"
